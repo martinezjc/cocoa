552eff11b7a239449c090ee0620a8fe182ad1754
Juan Martinez <martinezjc2011@gmail.com>
2015-11-21 10:53:22 -0600

    Add mysql2 to GemFile and update schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131128070808) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: true do |t|
     t.integer  "id_user_type"
     t.string   "identification"
